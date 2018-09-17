218368f685da829939baf072054971ff910770be
EddyK28 <EddyK28.yt@gmail.com>
2018-09-17 08:43:34 -0700

    Removed end of file junk, fixed a comment

--- RomFS/script/mods/Example/main.lua
+++ RomFS/script/mods/Example/main.lua
@@ -2,7 +2,7 @@
 
 text = "Hi there!\n I'm a sample mod!"
 
---many functions that take strings strip spaces. Replace spaces with equivalent _________
+--many functions that take strings strip spaces. Replace spaces with equivalent text directive
 text:gsub(" ","[LE]") 
 
 WINDOW:SysMsg(text)
